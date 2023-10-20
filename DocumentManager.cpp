8ccbd58f509a838f5e9dd001793540201ab1d46d
KennyLuong02 <126566057+KennyLuong02@users.noreply.github.com>
2023-10-20 14:50:40 +1030

    Add files via upload

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -30,6 +30,6 @@ bool DocumentManager::borrowDocument(int docid, int patronID) {
 }
 
 void DocumentManager::returnDocument(int docid, int patronID) {
-    mapID[docid] = mapID[docid]++;
+    mapID[docid]++;
     // mapPatron[patronID] = 0;
 }
